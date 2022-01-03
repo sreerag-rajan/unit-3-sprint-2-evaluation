19dd913da8e1be3d5e199a182ed403df4b9d078e
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-01-03 16:44:44 +0530

    9th commit

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,5 +3,18 @@ document.querySelector("#addressForm").addEventListener("submit", finalprocess);
 function finalprocess(event){
     event.preventDefault();
     alert("Your ORder is accepted");
-    
+
+    setTimeout(function(){
+        alert("Your Order is being cooked")
+    },3000)
+    setTimeout(function(){
+        alert("YOur order is ready")
+    },8000)
+    setTimeout(function(){
+        alert("Order out for delivery");
+    },10000)
+    setTimeout(function(){
+        alert("Order Delivered")
+    },12000)
+
 }
