d459c68e46572ef65ba238820a28f3ee68a62dd3
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-01-03 16:39:35 +0530

    8th commit

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,24 +1,50 @@
 let cart = JSON.parse(localStorage.getItem("mealCart"))
 
 function displayCart(){
-    for(let i =0; i<cart.length; i++){
-        console.log(cart[i]);
+    cart.map(function(el,index){
         let row = document.createElement("tr");
         let td1 = document.createElement("td");
         let img = document.createElement("img");
-        img.src = cart[i].meal.strMealThumb;
+        img.src = el.meal.strMealThumb;
         td1.append(img);
         let td2 = document.createElement("td");
         let n = document.createElement("h3");
-        n.textContent = cart[i].meal.strMeal;
+        n.textContent = el.meal.strMeal;
         td2.append(n);
         let td3 = document.createElement("td");
         let price = document.createElement("p");
-        price.textContent = cart[i].price;
-        row.append(td1,td2,td3);
+        price.textContent = el.price;
+        td3.append(price);
+
+        let td4 = document.createElement("td");
+        let rmv = document.createElement("button");
+        rmv.textContent = "remove";
+        rmv.onclick = function(){
+            remove(index);            
+        }
+        td4.append(rmv)
+        row.append(td1,td2,td3,td4);
         
         document.querySelector("table").append(row)
 
-    }
+    })
+       
 }
 displayCart()
+
+function total(){
+    let sum = 0;
+    for(let i =0; i<cart.length; i++){
+        sum+=cart[i].price;
+    }
+    return sum;
+}
+
+function remove(i){
+    console.log(i)
+    cart.splice(i,1);
+    localStorage.setItem("mealCart",JSON.stringify(cart));
+    displayCart();
+}
+
+document.querySelector("#tot").textContent =`Total: ${total()}`;
---
+++ scripts/checkout.js
@@ -0,0 +1,7 @@
+document.querySelector("#addressForm").addEventListener("submit", finalprocess);
+
+function finalprocess(event){
+    event.preventDefault();
+    alert("Your ORder is accepted");
+    
+}
