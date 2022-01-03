7e05c2ac0d888bff6ed4462070e3da7fe287cf60
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-01-03 14:47:24 +0530

    third commit

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,12 @@
 
 async function getMeal(){
     try{
-        let res = await fetch("www.themealdb.com/api/json/v1/1/search.php?f=a");
+        let res = await fetch("www.themealdb.com/api/json/v1/1/random.php");
         let data = await res.json();
-        console.log(res);
+        console.log(data);
     }
     catch(err){
         console.log(err)
     }
     
 }
-
-getMeal()
