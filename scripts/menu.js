f432f83641c0608182f62941ef8590accfa89344
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-01-03 15:52:14 +0530

    5th commit

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,12 +1,33 @@
+let menu =[];
 async function getMeal(){
     try{
-        let url = "www.themealdb.com/api/json/v1/1/random.php";
-        let res = await fetch(url);
-        // let data = await res.json();
-        console.log(res);
+        
+        for(let i =0; i<10; i++){
+            let res = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
+            let data = await res.json();
+            menu.push(data.meals);
+
+        }
+        console.log(menu)
+        
     }
     catch(err){
         console.log(err)
     }    
 }
 getMeal()
+
+function displayMenu(arr){
+    arr.map(function(el){
+        let img = document.createElement("img");
+        img.src = el.strMealThumb
+
+        let p = Math.floor((Math.random()*400))+100
+        let price = document.createElement("p");
+        price.textContent = p;
+
+        let addcart = document.createElement("button")
+        
+    })
+
+}
